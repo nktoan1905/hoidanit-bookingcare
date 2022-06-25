52d8238e0c88f0309655e0f54dca06c9a9b0ce83
nktoan1905 <nktoan1905@gmail.com>
2022-06-26 00:00:54 +0700

    frontend + connectDB

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
-
+import connectDB from "./config/connectDB";
 require("dotenv").config();
 
 let app = express();
@@ -14,8 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+connectDB();
 
 app.listen(port, () => {
   // call back
   console.log("Backend nodejs is running on the port: " + port);
 });
+
